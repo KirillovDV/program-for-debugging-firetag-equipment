7e8e2f45503dad606a60c4114a978650f5221a92
DanFleishman <KirillovDV61@obr.mos.ru>
2021-01-05 20:29:02 +0300

    Program update

--- PyProgram/main.py
+++ PyProgram/main.py
@@ -1,1 +1,45 @@
-# main.py
+from serial_port_scanner import serial_scanner
+
+def menu():
+    while True:
+        menu_choice = input('''
+Для начала настройки введите 'start'
+Для получения справки введите 'help'
+Для выхода введите 'exit'
+    
+——> ''')
+        menu_choice = menu_choice.lower()
+
+        if not menu_choice:
+            print('Вы ничего не ввели')
+
+        if menu_choice == 'exit':
+            exit()
+
+        if menu_choice == 'start':
+            print('QQ')
+            break
+
+        else:
+            print('Повторите ввод')
+
+
+def main():
+    menu_choice = input('''
+Для вывода доступных COM портов введите 'сom' 
+
+    ——> ''')
+    menu_choice = menu_choice.lower()
+    if menu_choice == 'com':
+        serial_scanner()
+
+
+def help():
+    pass
+
+# def help():
+#     pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ PyProgram/serial_port_scanner.py
@@ -0,0 +1,15 @@
+def serial_scanner():
+    import serial.tools.list_ports
+
+    list_of_serial_ports = serial.tools.list_ports.comports()
+    connected = []
+    str(connected)
+    for element in list_of_serial_ports:
+        connected.append(element.device)
+    # connected = str(connected)
+    for element in connected:
+        print(element)
+
+
+if __name__ == '__main__':
+    serial_scanner()
