6623aa86efd2549ccbf60257e7233f4e9b5940f9
DanFleishman <KirillovDV61@obr.mos.ru>
2021-01-09 00:40:17 +0300

    minor update

--- PyProgram/main.py
+++ PyProgram/main.py
@@ -1,38 +1,32 @@
-import serial_port_scanner
+
+from colorama import Fore, Back, Style
 from serial_port_scanner import serial_scanner
 
-# import os
+from menus import main_menu
+import os
+
+
+# —————————————————————— Main (first) menu of the program ——————————————————————
 
 
 def menu():
     while True:
-        menu_choice = input('''
-____________________________________________
-|                                          |
-|   ,--.   ,--.                            |
-|   |   `.'   | ,---. ,--,--, ,--.,--.     |
-|   |  |'.'|  || .-. :|      \|  ||  |     |
-|   |  |   |  |\   --.|  ||  |'  ''  '     |
-|   `--'   `--' `----'`--''--' `----'      |
-|                                          |
-|                                          |
-|  Для начала настройки введите 'start'    |
-|  Для получения справки введите 'help'    |
-|  Для выхода из программы введите 'exit'  |
-|__________________________________________|    
-    
-——> ''')
+        menu_choice = input(Fore.WHITE + main_menu)
         menu_choice = menu_choice.lower()
 
         if menu_choice == 'exit':
+            os.system('cls' if os.name == 'nt' else 'clear')
             exit()
 
         if menu_choice == 'start':
+            os.system('cls' if os.name == 'nt' else 'clear')
             main()
             break
 
         else:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Введено некорректное значение")
+            menu()
 
 
 def main():
@@ -46,17 +40,21 @@ def main():
 ——> ''')
         menu_choice = menu_choice.lower()
         if menu_choice == 'com':
-            print(serial_scanner())
+            os.system('cls' if os.name == 'nt' else 'clear')
+            comports()
             main()
 
         if menu_choice == 'start':
+            os.system('cls' if os.name == 'nt' else 'clear')
             start()
             break
 
         if menu_choice == 'exit':
+            os.system('cls' if os.name == 'nt' else 'clear')
             exit()
 
         if menu_choice == 'menu':
+            os.system('cls' if os.name == 'nt' else 'clear')
             menu()
             break
 
@@ -64,19 +62,23 @@ def main():
             print("Введено некорректное значение")
 
 
+def comports():
+    for element in serial_scanner():
+        print(Fore.RED + element, Fore.WHITE)
+
+
 def start():
 
-    def choose(connected):
+    def selector(connected):
         for i, element in enumerate(connected):
-            print(f'Введите {i + 1} если хотите выбрать  СОМ-порт {element} ')
+            print(Fore.WHITE + f'Введите {i + 1} если хотите выбрать  СОМ-порт {Fore.RED + element}', Fore.WHITE)
         number_com = int(input())
         return connected[number_com - 1]
 
-    choose_com = choose(serial_scanner())
-    selected_port = choose_com
-    print(f'Вы выбрали {selected_port}')
+    selected_port = selector(serial_scanner())
+    print(f'Вы выбрали {Fore.RED + selected_port}', Fore.WHITE)
 
-
+    main()
 
 
 def help():
