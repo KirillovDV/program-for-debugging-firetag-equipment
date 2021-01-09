05887d502dc50cddbfcb9386d47cd1c87baeff24
DanFleishman <KirillovDV61@obr.mos.ru>
2021-01-09 08:56:30 +0300

    minor update

--- PyProgram/main.py
+++ PyProgram/main.py
@@ -1,8 +1,6 @@
-
-from colorama import Fore, Back, Style
+from colorama import Fore
 from serial_port_scanner import serial_scanner
-
-from menus import main_menu
+from menus import main_menu, second_menu
 import os
 
 
@@ -25,19 +23,13 @@ def menu():
 
         else:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Введено некорректное значение")
+            print(Fore.RED + "Введено некорректное значение", Fore.WHITE)
             menu()
 
 
 def main():
     while True:
-        menu_choice = input('''
-Для вывода доступных COM портов введите 'сom' 
-Для начала настройки платы введиете 'start'
-Для выхода из программы введите 'exit'
-Для выхода в меню введите 'menu'
-    
-——> ''')
+        menu_choice = input(second_menu)
         menu_choice = menu_choice.lower()
         if menu_choice == 'com':
             os.system('cls' if os.name == 'nt' else 'clear')
@@ -68,22 +60,45 @@ def comports():
 
 
 def start():
+    print('Сейчас вам будет предложенно выбрать COM (serial) порт, к которому подключено ваше устройство. '
+          'Далее вы перейдете к настройке устройства. \nЕсли вы не увидите подключенное устройство, введите \'help\''
+          'и следуйте инструкциям\n\n')
 
     def selector(connected):
         for i, element in enumerate(connected):
-            print(Fore.WHITE + f'Введите {i + 1} если хотите выбрать  СОМ-порт {Fore.RED + element}', Fore.WHITE)
+            print(Fore.WHITE + f'Введите {i + 1} если хотите выбрать СОМ-порт {Fore.RED + element}', Fore.WHITE)
         number_com = int(input())
         return connected[number_com - 1]
 
     selected_port = selector(serial_scanner())
     print(f'Вы выбрали {Fore.RED + selected_port}', Fore.WHITE)
 
-    main()
-
 
 def help():
     print("справка по работе программы")
+    print("")
 
 
 if __name__ == '__main__':
-    menu()
+    import time
+    import serial
+    import datetime
+
+    ser = serial.Serial('/dev/cu.usbserial-1410')
+    received = []
+
+    ser.write(b'begin\n')
+    time.sleep(5)
+
+    start = datetime.datetime.now()
+    for i in range(1, 100):
+        ser.write(b'test%i\r\n' % i)
+        time.sleep(0.005)
+        while ser.inWaiting() > 0:
+            line = ser.readline()
+            if line:
+                received.append(line.decode().strip())
+
+    print(datetime.datetime.now() - start)
+    print(received)
+    # start()
--- PyProgram/tests.py
+++ PyProgram/tests.py
@@ -7,7 +7,17 @@ def serial_scanner():
     for element in list_of_serial_ports:
         connected.append(element.device)
     # connected = str(connected)
-    print(f"Connected COM (serial) ports: {connected} ")
+    # for element in connected:
+    #     print(element)
+    return connected
+
+
+"""Первое правило - пользователь всегда долбоящер, который вбивает не те числа и все ломает
+Мы обрабатываем их ошибки ввода внутри функции - это другие типы данных или интовые числа,
+которые выходят за границы допустимых диапозонов. Именно для этого я ввел 
+флаг ошибки, ты будешь его принимать и зацикливать ввод данных пока пользователь
+не введет их правильно.
+"""
 
 
 # ID = 1-128 type: int
@@ -17,71 +27,65 @@ def serial_scanner():
 # HP = 1-256 type: int
 # capacity = 1-256 type: int
 # ammo = 1-256 type: int
-def black_box(ID, team, damage, FF, HP, capacity, ammo):
-    flag_error = False
-    # ID
-    if 0 < ID < 129:
-        a = bin(ID - 1)[2:]
-        ID = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите ID в диапазоне 1-128')
-    # team
-    if 0 < team < 5:
-        a = bin(team - 1)[2:]
-        team = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите команду в диапазоне 1-4')
-    # damage
-    mas = [1, 2, 4, 5, 7, 10, 15, 17, 20, 25, 30, 35, 40, 50, 75, 100]
-    if damage in mas:
-        a = bin(mas.index(damage))[2:]
-        damage = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите урон в диапазоне 1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100')
-    # FF
-    if FF in [0, 1]:
-        a = bin(FF)[2:]
-        FF = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите дружественный огонь в диапазоне 0-1')
-    # HP
-    if 0 < HP < 257:
-        a = bin(HP - 1)[2:]
-        HP = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите ко-во жизней в диапазоне 1-256')
-    # capasity
-    if 0 < capacity < 257:
-        a = bin(capacity - 1)[2:]
-        capacity = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите ко-во патронов в магазине в диапазоне 1-256')
-    # ammo
-    if 0 < ammo < 257:
-        a = bin(ammo - 1)[2:]
-        ammo = '0' * (8 - len(a)) + a
-    else:
-        flag_error = True
-        print('Введите ко-во магазинов в диапазоне 1-256')
 
-    return flag_error, [ID, team, damage, FF, HP, capacity, ammo]
+def input_numbers():
+    ID = input('Ведите ID (1-128): ——> ')
+    team = input('Ведите команду (1-4):  ')
+    damage = input('Ведите урон (1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100): ——> ')
+    ff = input('Ведите учет дружественного огня (0-1): ——> ')
+    hp = input('Ведите кол-во жизней (1-256): ——> ')
+    capacity = input('Ведите кол-во патронов в магазине (1-256): ——> ')
+    ammo = input('Ведите ко-во магазинов (1-256): ——> ')
+    return [ID, team, damage, ff, hp, capacity, ammo]
 
 
-def concatenation(P1, P2, P3, P4, I1):
-    P_b1 = bin(int(P1, 2))
-    P_b2 = bin(int(P2 + P3 + P4, 2))
-    P_b3 = bin(int(I1, 2))
-    return [P_b1, P_b2, P_b3]
-    # return bytes(int(P,2))
+def interpreter():
+    def numbers_to_bin(massive_variable):
+        # флаг ошибки
+        flag_error = False
+        # массив допустимых значений
+        massive_right = [range(1, 129),
+                         range(1, 5),
+                         [1, 2, 4, 5, 7, 10, 15, 17, 20, 25, 30, 35, 40, 50, 75, 100],
+                         [0, 1],
+                         range(1, 257),
+                         range(1, 257),
+                         range(1, 257)]
+        # массив для вывода ошибок
+        massive_print = ['>ID<', '>Команда<', '>Урон<', '>Дружественный огонь<',
+                         '>Ко-во жизней<', '>Ко-во патронов в магазине<', '>Ко-во магазинов<']
+        # цикл перебора переменных
+        for i, variable in enumerate(massive_variable):
+            try:
+                variable = int(variable)
+                if variable in massive_right[i]:
+                    # если число есть в диапазоне то обрабатываем его
+                    a = bin(massive_right[i].index(variable))[2:]  # переводим в двоичную систему и отсекаем 0b
+                    variable = '0' * (8 - len(a)) + a  # дописываем нули до 8 бит
+                    massive_variable[i] = variable  # записываем в тот же массив
+                else:
+                    # если нет то сообщаем об ошибке через флаг, даже если она всего лишь одна
+                    flag_error = True
+                    print('Паблитто Даунитто, ты ввел недопустимые значения в ', massive_print[i])
+            except ValueError:
+                print('Паблитто Даунитто, ты ввел недопустимый формат в ', massive_print[i])
 
+        return flag_error, massive_variable
 
-print(concatenation('00000001', '11', '0', '0101', '1101'))
+    # флаг для работы цикла
+    flag = 1
+    while flag == 1:
+        fl_er, massive = numbers_to_bin(input_numbers())
+
+        if fl_er:
+            # если функция выдала ошибку то мы спрашиваем у пользователя и вводим флаг,
+            # отвечающий за продолжения или выход из цикла
+            flag = int(input('Если вы хотите ввести данные заново введите 1, выйти - введите 0:  '))
+        else:
+            # если ошибки нет, то выходим из цикла
+            flag = 0
+
 
 if __name__ == '__main__':
-    serial_scanner()
+    interpreter()
+
