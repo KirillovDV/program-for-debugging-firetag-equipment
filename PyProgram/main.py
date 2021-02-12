259f51af094b9eea56de1a0922d0638453fca7c9
DanFleishman <KirillovDV61@obr.mos.ru>
2021-02-12 13:12:33 +0300

    add UPD for interpreter-UPD.py

--- PyProgram/main.py
+++ PyProgram/main.py
@@ -1,8 +1,7 @@
 from colorama import Fore, Style
-from serial_port_tools import serial_scanner, comports, com_writer
+from serial_port_tools import serial_scanner, comports, interpreter
 from literals import main_menu, second_menu, info
 import os
-import interpreter
 
 
 def clear():  # функция отчистки терминала/командной строки
@@ -16,7 +15,7 @@ clear()  # Отчистака терминала/командной строки
 def menu():  # Главное меню
 
     while True:
-        menu_choice = input(Fore.CYAN + main_menu)  # Запрос ввода пункта меню (внешний вид из фала literals.py)
+        menu_choice = input(Fore.WHITE + main_menu)  # Запрос ввода пункта меню (внешний вид из фала literals.py)
         menu_choice = menu_choice.lower()  # Приведение введенного значения к нижнему регистру
 
         if menu_choice == 'exit':  # Если введено "Exit"
@@ -35,7 +34,7 @@ def menu():  # Главное меню
 
         else:  # Если введенно некорректное значение
             clear()  # Отчистака терминала/командной строки
-            print(Fore.RED + "Введено некорректное значение", Fore.CYAN)
+            print(Fore.RED + "Введено некорректное значение", Fore.WHITE)
             menu()  # Вызов функции menu()
 
 
@@ -68,17 +67,62 @@ def main():  # Второе меню
 
         else:
             clear()  # Отчистака терминала/командной строки
-            print(Fore.RED + "Введено некорректное значение", Fore.CYAN)
+            print(Fore.RED + "Введено некорректное значение", Fore.WHITE)
+
+
+# def configurator(): print('Сейчас вам будет предложенно выбрать COM (serial) порт, к которому подключено ваше
+# устройство. ' 'Далее вы перейдете к настройке устройства. \nЕсли вы не увидите подключенное устройство,
+# введите \'info()\'' 'и следуйте инструкциям\n\n')
+#
+#     def selector(connected):
+#         for i, element in enumerate(connected):
+#             print(Fore.WHITE + f'Введите {i + 1} если хотите выбрать СОМ-порт {Fore.RED + element}', Fore.WHITE)
+#         try:
+#             number_com = int(input())
+#             if int(number_com) in range(1, len(connected) + 1):
+#                 print("Все норм")
+#                 return connected[int(number_com) - 1]
+#         except ValueError:
+#             return False
+#
+#     flag = 1
+#     while flag == 1:
+#         selected_port = selector(serial_scanner())
+#         if selected_port:
+#             flag = 0
+#             print(f'Вы выбрали {Fore.RED + selected_port}', Fore.WHITE)
+#         else:
+#             flag = 1
+#             print('Вы ввели некорректное значение. Исправте ошибку')
+#
+#     flag = 1
+#     fl_er, massive = interpreter.numbers_to_bin(interpreter.input_numbers())
+#     while flag == 1 and fl_er == True:
+#         # если функция выдала ошибку то мы спрашиваем у пользователя и вводим флаг,
+#         # отвечающий за продолжения или выход из цикла
+#         try:
+#             flag = int(input('Если вы хотите ввести данные заново введите 1, выйти - введите 0: ——> '))
+#         except ValueError:
+#             flag = 1
+#             print('Вы ввели некорректное значение')
+#         else:
+#             # если ошибки нет, то выходим из цикла
+#             flag = 1
+#             # TODO
+#         print(massive)
+#     input("Eckb pjbnnt lf tap enter")
+#     com_writer(massive, selected_port)
 
 
 def configurator():
     print('Сейчас вам будет предложенно выбрать COM (serial) порт, к которому подключено ваше устройство. '
-          'Далее вы перейдете к настройке устройства. \nЕсли вы не увидите подключенное устройство, введите \'info()\''
+          'Далее вы перейдете к настройке устройства. \nЕсли вы не увидите подключенное устройство, '
+          'введите \'info\' '
           'и следуйте инструкциям\n\n')
 
     def selector(connected):
         for i, element in enumerate(connected):
-            print(Fore.CYAN + f'Введите {i + 1} если хотите выбрать СОМ-порт {Fore.RED + element}', Fore.CYAN)
+            print(Fore.WHITE + f'Введите {i + 1} если хотите выбрать СОМ-порт {Fore.RED + element}', Fore.WHITE)
         try:
             number_com = int(input())
             if int(number_com) in range(1, len(connected) + 1):
@@ -92,28 +136,14 @@ def configurator():
         selected_port = selector(serial_scanner())
         if selected_port:
             flag = 0
-            print(f'Вы выбрали {Fore.RED + selected_port}', Fore.CYAN)
+            print(f'Вы выбрали {Fore.RED + selected_port}', Fore.WHITE)
         else:
             flag = 1
             print('Вы ввели некорректное значение. Исправте ошибку')
 
-    flag = 1
-    fl_er, massive = interpreter.numbers_to_bin(interpreter.input_numbers())
-    while flag == 1 and fl_er == True:
-        # если функция выдала ошибку то мы спрашиваем у пользователя и вводим флаг,
-        # отвечающий за продолжения или выход из цикла
-        try:
-            flag = int(input('Если вы хотите ввести данные заново введите 1, выйти - введите 0: ——> '))
-        except ValueError:
-            flag = 1
-            print('Вы ввели некорректное значение')
-        else:
-            # если ошибки нет, то выходим из цикла
-            flag = 1
-            # TODO
-        print(massive)
-    input("Eckb pjbnnt lf tap enter")
-    com_writer(massive, selected_port)
+    interpreter()
+
+
 
 
 def help():
@@ -125,5 +155,4 @@ def help():
 
 
 if __name__ == '__main__':
-    # menu()
-    configurator()
+    menu()
